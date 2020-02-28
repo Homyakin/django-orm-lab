606d64adb3c710d866f821c55ddd281bb1c7cc56
Homyakin <tox.home@ya.ru>
2020-02-28 18:21:28 +0300

    [NF] added flask-orator + requirements

--- storage-service/app/__init__.py
+++ storage-service/app/__init__.py
@@ -1,3 +1,15 @@
 from flask import Flask
+from flask_orator import Orator
 
 app = Flask(__name__)
+app.config['ORATOR_DATABASES'] = {
+    'development': {
+        'driver': 'mysql',
+        'host': 'localhost',
+        'database': 'storage',
+        'user': 'root',
+        'password': '12345'
+    }
+}
+
+db = Orator(app)
--- storage-service/run.py
+++ storage-service/run.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from app import view
 
 
 if __name__ == "__main__":
     app.run()
+    db.cli.run()
